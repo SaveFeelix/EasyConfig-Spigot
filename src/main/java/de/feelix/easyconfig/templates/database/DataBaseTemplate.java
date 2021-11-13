9f78ac8fef05961060ff7827a25d42327616d2f7
SaveFeelix <felixhake86@gmail.com>
2021-11-13 15:04:16 +0100

    Remove maven.yml Update DataBaseTemplate.java Try to fix WriteError in MessagesTemplate.java

--- src/main/java/de/feelix/easyconfig/templates/database/DataBaseTemplate.java
+++ src/main/java/de/feelix/easyconfig/templates/database/DataBaseTemplate.java
@@ -7,7 +7,9 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Class to create a DataBaseConfig
  */
-public abstract class DataBaseTemplate extends PluginConfig {
+public abstract class DataBaseTemplate extends de.feelix.easyconfig.templates.DataBaseTemplate {
+
+    protected final JavaPlugin plugin;
 
     /**
      * Default Constructor
@@ -23,118 +25,7 @@ public abstract class DataBaseTemplate extends PluginConfig {
      * @param folderName the folderName
      */
     public DataBaseTemplate(@NotNull JavaPlugin plugin, @NotNull String folderName) {
-        super(plugin, folderName, "database");
-    }
-
-    @Override
-    public void addDefault() {
-        this.add("isEnabled", false);
-        this.add("host", "localhost");
-        this.add("port", 3306);
-        this.add("database", "myDatabase");
-        this.add("user", "myUser");
-        this.add("password", "myPassword");
-        addOtherDefault();
-    }
-
-    /**
-     * Method to add other Values to the Config
-     */
-    public abstract void addOtherDefault();
-
-    /**
-     * Getter for isEnabled
-     * @return isEnabled
-     */
-    public boolean isEnabled() {
-        return this.getFromConfig("isEnabled");
-    }
-
-    /**
-     * Setter for isEnabled
-     * @param enabled isEnabled
-     */
-    public void isEnabled(boolean enabled) {
-        this.set("isEnabled", enabled);
-    }
-
-    /**
-     * Getter for the Host
-     * @return the Host
-     */
-    public String getHost() {
-        return this.getFromConfig("host");
-    }
-
-    /**
-     * Setter for the Host
-     * @param host the Host
-     */
-    public void setHost(String host) {
-        this.set("host", host);
-    }
-
-    /**
-     * Getter for the Database
-     * @return the DataBase
-     */
-    public String getDatabase() {
-        return this.getFromConfig("database");
-    }
-
-    /**
-     * Setter for the DataBase
-     * @param database the DataBase
-     */
-    public void setDatabase(String database) {
-        this.set("database", database);
-    }
-
-    /**
-     * Getter for the User
-     * @return the User
-     */
-    public String getUser() {
-        return this.getFromConfig("user");
-    }
-
-    /**
-     * Setter for the User
-     * @param user the User
-     */
-    public void setUser(String user) {
-        this.set("user", user);
-    }
-
-    /**
-     * Getter for the Password
-     * @return the Password
-     */
-    public String getPassword() {
-        return this.getFromConfig("password");
-    }
-
-    /**
-     * Setter for the Password
-     * @param password the Password
-     */
-    public void setPassword(String password) {
-        this.set("password", password);
-    }
-
-    /**
-     * Getter for the Port
-     * @return the Port
-     */
-    public int getPort() {
-        return this.getFromConfig("port");
-    }
-
-    /**
-     * Setter for the Port
-     * @param port the Port
-     */
-    public void setPort(int port) {
-        this.set("port", port);
+        super("plugins/" + plugin.getName() + "/" + folderName);
+        this.plugin = plugin;
     }
 }
--- src/main/java/de/feelix/easyconfig/templates/messages/MessagesTemplate.java
+++ src/main/java/de/feelix/easyconfig/templates/messages/MessagesTemplate.java
@@ -6,8 +6,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Objects;
-
 /**
  * Template to generate a Config with Messages
  */
@@ -20,6 +18,7 @@ public abstract class MessagesTemplate extends PluginConfig {
 
     /**
      * Constructor of MessagesTemplate
+     *
      * @param plugin reference to the Plugin
      */
     public MessagesTemplate(@NotNull JavaPlugin plugin) {
@@ -28,7 +27,8 @@ public abstract class MessagesTemplate extends PluginConfig {
 
     /**
      * Constructor of MessagesTemplate
-     * @param plugin reference to the Plugin
+     *
+     * @param plugin     reference to the Plugin
      * @param folderName the folderName
      */
     public MessagesTemplate(@NotNull JavaPlugin plugin, @NotNull String folderName) {
@@ -37,31 +37,30 @@ public abstract class MessagesTemplate extends PluginConfig {
 
     /**
      * Constructor of MessagesTemplate
-     * @param plugin reference to the Plugin
-     * @param folderName the folderName
+     *
+     * @param plugin         reference to the Plugin
+     * @param folderName     the folderName
      * @param prefixVariable the prefixVariable
      */
     public MessagesTemplate(@NotNull JavaPlugin plugin, @NotNull String folderName, @Nullable String prefixVariable) {
         super(plugin, folderName, "messages");
-        this.prefixVariable = Objects.requireNonNull(prefixVariable, "%prefix%");
+        this.prefixVariable = prefixVariable != null ? prefixVariable : "%prefix%";
+        addDefaultMessages();
     }
 
-    @Override
-    public void addDefault() {
+    /**
+     * Method to add default Messages
+     */
+    private void addDefaultMessages() {
         this.add("prefix", plugin.getName() + " §7>>");
         this.add("load", "%prefix% §6Plugin load§7...");
         this.add("enable", "%prefix% §aPlugin enabled");
         this.add("disable", "%prefix% §cPlugin disabled");
-        addOtherMessages();
     }
 
     /**
-     * Abstract method to add other Default values
-     */
-    public abstract void addOtherMessages();
-
-    /**
      * Getter for the Prefix
+     *
      * @return the Prefix
      */
     public String getPrefix() {
@@ -70,6 +69,7 @@ public abstract class MessagesTemplate extends PluginConfig {
 
     /**
      * Getter for the LoadMessage
+     *
      * @return the LoadMessage as a String
      */
     public String getLoadString() {
@@ -78,6 +78,7 @@ public abstract class MessagesTemplate extends PluginConfig {
 
     /**
      * Getter for the EnableMessage
+     *
      * @return the EnableMessage as a String
      */
     public String getEnableString() {
@@ -86,9 +87,10 @@ public abstract class MessagesTemplate extends PluginConfig {
 
     /**
      * Getter for the DisableMessage
+     *
      * @return the DisableMessage as a String
      */
     public String getDisableString() {
-        return TextUtils.checkOther(this.getFromConfig("disable"), "%prefix%", this.getPrefix());
+        return TextUtils.checkOther(this.getFromConfig("disable"), prefixVariable, this.getPrefix());
     }
 }
